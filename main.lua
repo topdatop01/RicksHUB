60d8ab733432a9d62722a7ce3497af3086715816
topdatop01 <schilingl@yandex.ru>
2025-10-13 21:36:44 +0300

    Сделана обфусикация

--- main.lua
+++ main.lua
@@ -1,202 +1,201 @@
-local http = game:GetService("HttpService")
-local scripts = game:HttpGet("https://raw.githubusercontent.com/topdatop01/RicksHUB/refs/heads/main/script.json")
-scripts = http:JSONDecode(scripts)
-for _, v in pairs(scripts)
-	v.name = v.name:gsub("(ЛУЧШЕЕ!)", '<font color="#FFA500">(ЛУЧШЕЕ!)</font>')
-	v.name = v.name:gsub("(ЛУЧШИЙ В СВОЕЙ КАТЕГОРИИ!)", '<font color="#FFA500">(ЛУЧШИЙ В СВОЕЙ КАТЕГОРИИ!)</font>')
+local a=game:GetService"HttpService"
+local b=game:HttpGet"https://raw.githubusercontent.com/topdatop01/RicksHUB/refs/heads/main/script.json"
+b=a:JSONDecode(b)
+for c,d in pairs(b)do
+d.name=d.name:gsub('(ЛУЧШЕЕ!)','<font color="#FFA500">(ЛУЧШЕЕ!)</font>')
+d.name=d.name:gsub('(ЛУЧШИЙ В СВОЕЙ КАТЕГОРИИ!)','<font color="#FFA500">(ЛУЧШИЙ В СВОЕЙ КАТЕГОРИИ!)</font>')
 end
-scripts = scripts:gsub("(ЛУЧШЕЕ!)", '<font color="#FFA500">(ЛУЧШЕЕ!)</font>')
-local RicksHUB = Instance.new("ScreenGui")
-local MainFrame = Instance.new("ImageLabel")
-local ScrollingFrame = Instance.new("ScrollingFrame")
-local UIListLayout = Instance.new("UIListLayout")
-local TitleLabel = Instance.new("TextLabel")
-local SearchBox = Instance.new("TextBox")
-local CloseButton = Instance.new("TextButton")
-local Exploit = Instance.new("Frame")
-local Title = Instance.new("TextLabel")
-local Execute = Instance.new("TextButton")
-local Description = Instance.new("TextLabel")
+local c=Instance.new"ScreenGui"
+local d=Instance.new"ImageLabel"
+local e=Instance.new"ScrollingFrame"
+local f=Instance.new"UIListLayout"
+local g=Instance.new"TextLabel"
+local h=Instance.new"TextBox"
+local i=Instance.new"TextButton"
+local j=Instance.new"Frame"
+local k=Instance.new"TextLabel"
+local l=Instance.new"TextButton"
+local m=Instance.new"TextLabel"
 
---Properties:
 
-RicksHUB.Name = math.random(0, 9) .. math.random(0, 9) .. math.random(0, 9) .. math.random(0, 9) .. math.random(0, 9) .. math.random(0, 9) .. math.random(0, 9)
-RicksHUB.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-RicksHUB.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-RicksHUB.IgnoreGuiInset = true
-RicksHUB.ResetOnSpawn = false
-RicksHUB.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
 
-MainFrame.Name = "MainFrame"
-MainFrame.Parent = RicksHUB
-MainFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-MainFrame.BackgroundTransparency = 0.450
-MainFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-MainFrame.BorderSizePixel = 0
-MainFrame.Size = UDim2.new(1, 0, 1, 0)
-MainFrame.Image = "rbxassetid://143854825"
+c.Name=math.random(0,9)..math.random(0,9)..math.random(0,9)..math.random(0,9)..math.random(0,9)..math.random(0,9)..math.random(0,9)
+c.Parent=game.Players.LocalPlayer:WaitForChild"PlayerGui"
+c.ZIndexBehavior=Enum.ZIndexBehavior.Sibling
+c.IgnoreGuiInset=true
+c.ResetOnSpawn=false
+c.ScreenInsets=Enum.ScreenInsets.DeviceSafeInsets
 
-ScrollingFrame.Parent = MainFrame
-ScrollingFrame.Active = true
-ScrollingFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ScrollingFrame.BackgroundTransparency = 1.000
-ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ScrollingFrame.BorderSizePixel = 0
-ScrollingFrame.Position = UDim2.new(0, 0, 0.100000001, 0)
-ScrollingFrame.Size = UDim2.new(1, 0, 0.699999988, 0)
-ScrollingFrame.CanvasSize = UDim2.new(0, 0, 1, 0)
-ScrollingFrame.ScrollBarThickness = 6
+d.Name="MainFrame"
+d.Parent=c
+d.BackgroundColor3=Color3.fromRGB(0,0,0)
+d.BackgroundTransparency=0.450
+d.BorderColor3=Color3.fromRGB(0,0,0)
+d.BorderSizePixel=0
+d.Size=UDim2.new(1,0,1,0)
+d.Image="rbxassetid://143854825"
 
-UIListLayout.Parent = ScrollingFrame
-UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-UIListLayout.Padding = UDim.new(0, 20)
+e.Parent=d
+e.Active=true
+e.BackgroundColor3=Color3.fromRGB(255,255,255)
+e.BackgroundTransparency=1.000
+e.BorderColor3=Color3.fromRGB(0,0,0)
+e.BorderSizePixel=0
+e.Position=UDim2.new(0,0,0.100000001,0)
+e.Size=UDim2.new(1,0,0.699999988,0)
+e.CanvasSize=UDim2.new(0,0,1,0)
+e.ScrollBarThickness=6
 
-TitleLabel.Name = "TitleLabel"
-TitleLabel.Parent = MainFrame
-TitleLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TitleLabel.BackgroundTransparency = 1.000
-TitleLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TitleLabel.BorderSizePixel = 0
-TitleLabel.Position = UDim2.new(0.100000001, 0, 0, 0)
-TitleLabel.Size = UDim2.new(0.800000012, 0, 0.100000001, 0)
-TitleLabel.Font = Enum.Font.GothamBlack
-TitleLabel.Text = "RicksHUB (TG: @ricksscripters)"
-TitleLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TitleLabel.TextScaled = true
-TitleLabel.TextSize = 14.000
-TitleLabel.TextWrapped = true
+f.Parent=e
+f.SortOrder=Enum.SortOrder.LayoutOrder
+f.Padding=UDim.new(0,20)
 
-SearchBox.Name = "SearchBox"
-SearchBox.Parent = MainFrame
-SearchBox.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
-SearchBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-SearchBox.BorderSizePixel = 0
-SearchBox.Position = UDim2.new(0, 0, 0.800000012, 0)
-SearchBox.Size = UDim2.new(1, 0, 0.100000001, 0)
-SearchBox.ClearTextOnFocus = false
-SearchBox.Font = Enum.Font.SourceSans
-SearchBox.PlaceholderText = "Name or description"
-SearchBox.Text = ""
-SearchBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-SearchBox.TextScaled = true
-SearchBox.TextSize = 14.000
-SearchBox.TextWrapped = true
+g.Name="TitleLabel"
+g.Parent=d
+g.BackgroundColor3=Color3.fromRGB(255,255,255)
+g.BackgroundTransparency=1.000
+g.BorderColor3=Color3.fromRGB(0,0,0)
+g.BorderSizePixel=0
+g.Position=UDim2.new(0.100000001,0,0,0)
+g.Size=UDim2.new(0.800000012,0,0.100000001,0)
+g.Font=Enum.Font.GothamBlack
+g.Text="RicksHUB (TG: @ricksscripters)"
+g.TextColor3=Color3.fromRGB(255,255,255)
+g.TextScaled=true
+g.TextSize=14.000
+g.TextWrapped=true
 
-CloseButton.Name = "CloseButton"
-CloseButton.Parent = MainFrame
-CloseButton.BackgroundColor3 = Color3.fromRGB(223, 223, 223)
-CloseButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-CloseButton.BorderSizePixel = 0
-CloseButton.Position = UDim2.new(0, 0, 0.899999976, 0)
-CloseButton.Size = UDim2.new(1, 0, 0.100000001, 0)
-CloseButton.Font = Enum.Font.Gotham
-CloseButton.Text = "Close HUB"
-CloseButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-CloseButton.TextScaled = true
-CloseButton.TextSize = 14.000
-CloseButton.TextWrapped = true
+h.Name="SearchBox"
+h.Parent=d
+h.BackgroundColor3=Color3.fromRGB(90,90,90)
+h.BorderColor3=Color3.fromRGB(0,0,0)
+h.BorderSizePixel=0
+h.Position=UDim2.new(0,0,0.800000012,0)
+h.Size=UDim2.new(1,0,0.100000001,0)
+h.ClearTextOnFocus=false
+h.Font=Enum.Font.SourceSans
+h.PlaceholderText="Name or description"
+h.Text=""
+h.TextColor3=Color3.fromRGB(255,255,255)
+h.TextScaled=true
+h.TextSize=14.000
+h.TextWrapped=true
 
-Exploit.Name = "Exploit"
-Exploit.Parent = MainFrame
-Exploit.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Exploit.BackgroundTransparency = 0.650
-Exploit.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Exploit.BorderSizePixel = 0
-Exploit.Size = UDim2.new(1, 0, 0, 100)
-Exploit.Visible = false
+i.Name="CloseButton"
+i.Parent=d
+i.BackgroundColor3=Color3.fromRGB(223,223,223)
+i.BorderColor3=Color3.fromRGB(0,0,0)
+i.BorderSizePixel=0
+i.Position=UDim2.new(0,0,0.899999976,0)
+i.Size=UDim2.new(1,0,0.100000001,0)
+i.Font=Enum.Font.Gotham
+i.Text="Close HUB"
+i.TextColor3=Color3.fromRGB(0,0,0)
+i.TextScaled=true
+i.TextSize=14.000
+i.TextWrapped=true
 
-Title.Name = "Title"
-Title.Parent = Exploit
-Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Title.BackgroundTransparency = 1.000
-Title.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Title.BorderSizePixel = 0
-Title.Size = UDim2.new(0.850000024, 0, 0.5, 0)
-Title.Font = Enum.Font.GothamBold
-Title.Text = "TDTSpeed"
-Title.TextColor3 = Color3.fromRGB(255, 255, 255)
-Title.TextScaled = true
-Title.TextSize = 14.000
-Title.TextStrokeTransparency = 0.000
-Title.TextWrapped = true
-Title.TextXAlignment = Enum.TextXAlignment.Left
-Title.RichText = true
+j.Name="Exploit"
+j.Parent=d
+j.BackgroundColor3=Color3.fromRGB(0,0,0)
+j.BackgroundTransparency=0.650
+j.BorderColor3=Color3.fromRGB(0,0,0)
+j.BorderSizePixel=0
+j.Size=UDim2.new(1,0,0,100)
+j.Visible=false
 
-Execute.Name = "Execute"
-Execute.Parent = Exploit
-Execute.BackgroundColor3 = Color3.fromRGB(223, 223, 223)
-Execute.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Execute.BorderSizePixel = 0
-Execute.Position = UDim2.new(0.850000024, 0, 0, 0)
-Execute.Size = UDim2.new(0.150000006, 0, 1, 0)
-Execute.Font = Enum.Font.Gotham
-Execute.Text = "Execute"
-Execute.TextColor3 = Color3.fromRGB(0, 0, 0)
-Execute.TextScaled = true
-Execute.TextSize = 14.000
-Execute.TextWrapped = true
+k.Name="Title"
+k.Parent=j
+k.BackgroundColor3=Color3.fromRGB(255,255,255)
+k.BackgroundTransparency=1.000
+k.BorderColor3=Color3.fromRGB(0,0,0)
+k.BorderSizePixel=0
+k.Size=UDim2.new(0.850000024,0,0.5,0)
+k.Font=Enum.Font.GothamBold
+k.Text="TDTSpeed"
+k.TextColor3=Color3.fromRGB(255,255,255)
+k.TextScaled=true
+k.TextSize=14.000
+k.TextStrokeTransparency=0.000
+k.TextWrapped=true
+k.TextXAlignment=Enum.TextXAlignment.Left
+k.RichText=true
 
-Description.Name = "Description"
-Description.Parent = Exploit
-Description.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Description.BackgroundTransparency = 1.000
-Description.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Description.BorderSizePixel = 0
-Description.Position = UDim2.new(0, 0, 0.5, 0)
-Description.Size = UDim2.new(0.850000024, 0, 0.5, 0)
-Description.Font = Enum.Font.Gotham
-Description.Text = "Hack your speed"
-Description.TextColor3 = Color3.fromRGB(255, 255, 255)
-Description.TextScaled = true
-Description.TextSize = 14.000
-Description.TextStrokeTransparency = 0.000
-Description.TextWrapped = true
-Description.TextXAlignment = Enum.TextXAlignment.Left
-Description.RichText = true
+l.Name="Execute"
+l.Parent=j
+l.BackgroundColor3=Color3.fromRGB(223,223,223)
+l.BorderColor3=Color3.fromRGB(0,0,0)
+l.BorderSizePixel=0
+l.Position=UDim2.new(0.850000024,0,0,0)
+l.Size=UDim2.new(0.150000006,0,1,0)
+l.Font=Enum.Font.Gotham
+l.Text="Execute"
+l.TextColor3=Color3.fromRGB(0,0,0)
+l.TextScaled=true
+l.TextSize=14.000
+l.TextWrapped=true
 
--- Scripts:
+m.Name="Description"
+m.Parent=j
+m.BackgroundColor3=Color3.fromRGB(255,255,255)
+m.BackgroundTransparency=1.000
+m.BorderColor3=Color3.fromRGB(0,0,0)
+m.BorderSizePixel=0
+m.Position=UDim2.new(0,0,0.5,0)
+m.Size=UDim2.new(0.850000024,0,0.5,0)
+m.Font=Enum.Font.Gotham
+m.Text="Hack your speed"
+m.TextColor3=Color3.fromRGB(255,255,255)
+m.TextScaled=true
+m.TextSize=14.000
+m.TextStrokeTransparency=0.000
+m.TextWrapped=true
+m.TextXAlignment=Enum.TextXAlignment.Left
+m.RichText=true
 
-local function SAUP_fake_script() -- MainFrame.LocalScript 
-	local script = Instance.new('LocalScript', MainFrame)
 
-	local textBox = script.Parent.SearchBox
-	local scriptsFrame = script.Parent.ScrollingFrame
-	local template = script.Parent.Exploit
-	
-	for _, v in pairs(scripts) do
-		local new = template:Clone()
-		new.Title.Text = v.name
-		new.Description.Text = v.description
-		new.Execute.MouseButton1Click:Connect(function()
-			local scripto = v.code
-			scripto()
-		end)
-		new.Visible = true
-		new.Parent = scriptsFrame
-	end
-	
-	textBox.Changed:Connect(function()
-		if textBox.Text ~= "" then
-			for _, v in pairs(scriptsFrame:GetChildren()) do
-				if v:IsA("Frame") then
-					if v.Title.Text:lower():find(textBox.Text:lower()) or v.Description.Text:lower():find(textBox.Text:lower()) then
-						v.Visible = true
-					else
-						v.Visible = false
-					end
-				end
-			end
-		else
-			for _, v in pairs(scriptsFrame:GetChildren()) do
-				if v:IsA("Frame") then
-					v.Visible = true
-				end
-			end
-		end
-	end)
-	
-	script.Parent.CloseButton.MouseButton1Click:Connect(function()
-		script.Parent.Parent:Destroy()
-	end)
+
+local function SAUP_fake_script()
+local n=Instance.new('LocalScript',d)
+
+local o=n.Parent.SearchBox
+local p=n.Parent.ScrollingFrame
+local q=n.Parent.Exploit
+
+for r,s in pairs(b)do
+local t=q:Clone()
+t.Title.Text=s.name
+t.Description.Text=s.description
+t.Execute.MouseButton1Click:Connect(function()
+local u=s.code
+u()
+end)
+t.Visible=true
+t.Parent=p
+end
+
+o.Changed:Connect(function()
+if o.Text~=""then
+for r,s in pairs(p:GetChildren())do
+if s:IsA"Frame"then
+if s.Title.Text:lower():find(o.Text:lower())or s.Description.Text:lower():find(o.Text:lower())then
+s.Visible=true
+else
+s.Visible=false
+end
+end
+end
+else
+for r,s in pairs(p:GetChildren())do
+if s:IsA"Frame"then
+s.Visible=true
+end
+end
+end
+end)
+
+n.Parent.CloseButton.MouseButton1Click:Connect(function()
+n.Parent.Parent:Destroy()
+end)
 end
 coroutine.wrap(SAUP_fake_script)()
